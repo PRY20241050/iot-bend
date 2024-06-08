ede4a5c884ecea26835f950891cecd5b8dc4a54e
havuh <alejandr.jacobo@gmail.com>
2024-06-08 02:34:35 -0500

    feat: added register user

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('core.users.urls')),
 ]
 
