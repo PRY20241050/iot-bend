892888054d1c3841750c82269304c3fe8acc5954
havuh <alejandr.jacobo@gmail.com>
2024-10-21 14:08:39 -0500

    fix: brickyard order by was updated

--- src/core/api/views/brickyard_view.py
+++ src/core/api/views/brickyard_view.py
@@ -9,7 +9,7 @@ class BrickyardListCreateView(ListCreateAPIView):
     Handle GET and POST requests for brickyard data.
     """
 
-    queryset = Brickyard.objects.filter(visible=True)
+    queryset = Brickyard.objects.filter(visible=True).order_by("pk")
     serializer_class = BrickyardSerializer
     permission_classes = [IsAuthenticated]
 
